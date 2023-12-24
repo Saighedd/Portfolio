32721f8d806d57b2a34160370194217c6aebe4b3
Saighed <140229643+Saighedd@users.noreply.github.com>
2023-12-24 15:09:17 +1100

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-<script>
-function cb(response) {
-document.getElementById('visits').innerText = response.value;
-}
-</script>
-<script async src="https://api.countabc.xyz/hit/saighed.xyz/visits?callback=cb"> </script>
